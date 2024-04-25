45f0f07c50617fd71b5a4c2abe3736ca7c62e81f
heba-arabiat <hebaarabiat1@gmail>
2024-04-25 09:41:50 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const mobilebtn = document.querySelector("#mobile-btn");
+const mobilemenu = document.querySelector("#mobile-menu");
+mobilebtn.addEventListener("click", ()=>{
+mobilemenu.classList.toggle("hidden");
+});
